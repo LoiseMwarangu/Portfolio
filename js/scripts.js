b1de8b41a8d2719a5974191a53dd7663494f506f
Loise Mwarangu <loisemwarangu@gmail.com>
2019-04-04 10:42:14 +0300

    added content to portfolio.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,66 @@
-/**
- * Website js scripts init
- */
+block = $('.blocks__block')
+bname = $('.blocks__name')
+content = $('.blocks-content__content')
+closeBtn = $('.blocks__content-close')
+wHeight = $(window).outerHeight()
+wWidth = $(window).outerWidth()
+bHeight = block.outerHeight()
+bWidth = block.outerWidth()
+xVal = Math.round(wWidth / bWidth) + 0.03
+yVal = wHeight / bHeight + 0.03
 
-jQuery( document ).ready(function($) {
-  'use strict';
+expand = ->
+  num = $(this).index()
+  aBlock = block.eq(num)
+  if !aBlock.hasClass('active')
+    bname.css 'opacity', '0'
+    aBlock.css
+      'transform': 'scale(' + xVal + ',' + yVal + ')'
+      '-webkit-transform': 'scale(' + xVal + ',' + yVal + ')'
+    aBlock.addClass 'active'
+    openContent num
+  return
 
+openContent = (num) ->
+  content.css
+    'transition': 'all 0.3s 0.4s ease-out'
+    '-webkit-transition': 'all 0.3s 0.4s ease-out'
+  aContent = content.eq(num)
+  aContent.addClass 'active'
+  return
 
-var $container = $('.masonry').imagesLoaded( function() {
-//var $container = $('.masonry');
-$container.imagesLoaded(function(){
-$container.masonry({
-  // options
-  columnWidth: 0,
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  //columnWidth: 200
-});});});
+closeContent = ->
+  bname.css 'opacity', '1'
+  content.css
+    'transition': 'all 0.1s 0 ease-out'
+    '-webkit-transition': 'all 0.1s 0 ease-out'
+  block.css
+    'transform': 'scale(1)'
+    '-webkit-transform': 'scale(1)'
+  block.removeClass 'active'
+  content.removeClass 'active'
+  return
 
-      // Show the load more button again
-      $( '#infinite-handle' ).fadeIn();
- 
-   // $( function() { // Ready
+updateValues = ->
+  `var yVal`
+  `var xVal`
+  `var bWidth`
+  `var bHeight`
+  `var wWidth`
+  `var wHeight`
+  if block.hasClass('active')
+    aBlock = $('.blocks__block.active')
+    wHeight = $(window).height()
+    wWidth = $(window).width()
+    bHeight = block.height()
+    bWidth = block.width()
+    xVal = Math.round(wWidth / bWidth) + 0.03
+    yVal = wHeight / bHeight + 0.03
+    aBlock.css
+      'transform': 'scale(' + xVal + ',' + yVal + ')'
+      '-webkit-transform': 'scale(' + xVal + ',' + yVal + ')'
+  return
 
-   //    var settings = { 
-   //        anchors: 'a',
-   //        blacklist: '.wp-link'
-   //    };
-
-  //     $( '#page' ).smoothState( settings );
-  // } );     
-} );
+$(window).on 'resize', updateValues
+bname.on 'click', expand
+closeBtn.on 'click', closeContent
